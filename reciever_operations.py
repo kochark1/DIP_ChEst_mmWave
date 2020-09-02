fixed scaling issues

reciever_operations.py
@@ -21,7 +21,8 @@ class ChannelEstimator:
         Datapath (equalization, demodulation and decoding)
     
     """
-    def __init__(self, system_parameters_passed, channel_parameters_passed):
+    def __init__(self, system_parameters_passed, channel_parameters_passed,
+                 snr_id):
         """
         
 
@@ -41,8 +42,9 @@ class ChannelEstimator:
         """
         self.system_parameters = system_parameters_passed
         self.channel_parameters = channel_parameters_passed
+        self.snr_id = snr_id
     
-    def estimate(self, y_matrix, h_matrix_org, snr_id):
+    def estimate(self, y_matrix, h_matrix_org):
         """
         
 
@@ -57,22 +59,25 @@ class ChannelEstimator:
 
         """
         def channel_estimation(self, y_matrix):
+            snr_dB = self.system_parameters.snr_dB[self.snr_id]
+            snr_lin = 10**(snr_dB/10)
             h_LS = np.matmul(y_matrix,
                              np.linalg.pinv(self.system_parameters.t_matrix))
-            # h_matrix = h_LS
+            h_LS = np.sqrt((snr_lin+1)/snr_lin)*h_LS
+            h_matrix = h_LS
             h_temp = dip_processing(h_LS, [8], 0, layers = 6, lr = 0.01 )
             h_matrix = h_temp['SNR0_k8']
             return h_matrix
         
         def nmse_calculator(h_est, h_org):
-            mse = np.mean((abs(h_est - h_org))**2)
-            den = np.mean((abs(h_org))**2)
+            mse = np.linalg.norm((h_est - h_org), 'fro')
+            den = np.linalg.norm(h_org, 'fro')
             if den <= (1e-7):
                 return mse*1e7
             return mse/den
         
-        def dip_processing(Y_input, out_channel_list = [8], SNR = 0, layers = 6,
-                           lr = 0.01 ):
+        def dip_processing(Y_input, out_channel_list = [8], SNR = 0,
+                           layers = 6, lr = 0.01 ):
             """
             Parameters:
             

systemSimulator.py
@@ -36,6 +36,7 @@ class SystemModel:
         self.snr_id = snr_id
     def generate(self):
         x_matrix_org = self.generateChannel()
+        x_matrix_org /= 16.5
         y_matrix = self.generateTXsignal(x_matrix_org)
         x_temp = np.matmul(SystemModel.b_left_matrix, x_matrix_org)
         h_matrix_org = np.matmul(x_temp, SystemModel.b_nt_matrix.conj().T)
@@ -46,9 +47,9 @@ class SystemModel:
         nR = self.system_parameters.nR
         x_matrix = 1j*(np.zeros((nR, nT *self.system_parameters.const_L)))
         for l in range(self.system_parameters.const_L):
-            temp = random(nR, nT, density=0.05).A +\
-                1j * random(nR, nT, density=0.05).A
-            x_matrix[:, (l*nT):((l+1)*nT)] = temp
+            real_mat = random(nR, nT, density=0.05).A
+            imag_mat = (real_mat>1e-2)*np.random.randn(nR,nT)
+            x_matrix[:, (l*nT):((l+1)*nT)] = real_mat + 1j * imag_mat
         return x_matrix
     def generateTXsignal(self, h_matrix):
         nT = self.system_parameters.nT
@@ -62,7 +63,8 @@ class SystemModel:
                              b_right_matrix)
         noise_matrix = (1/np.sqrt(2))*np.random.randn(nT, nP) +\
             1j * (1/np.sqrt(2))*np.random.randn(nT, nP)
-        y_matrix = z_matrix + (1/snr_lin)*noise_matrix
+        y_matrix = np.sqrt(snr_lin/(snr_lin+1))*z_matrix +\
+            np.sqrt(1/(snr_lin+1))*noise_matrix
         y_matrix = np.sign(y_matrix.real) + 1j * np.sign(y_matrix.imag)
         return y_matrix
     